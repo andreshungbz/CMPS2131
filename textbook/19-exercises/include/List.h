67779f6b544f6e486106d3e641da38d4b645506c
Andres Hung <andres.hung@outlook.com>
2024-08-26 16:40:02 -0600

    Refactor List class member functions

--- textbook/19-exercises/include/List.h
+++ textbook/19-exercises/include/List.h
@@ -17,154 +17,19 @@ class List {
     friend void merge(List<T>& a, const List<T>& b, const List<T>& c);
 
 public:
-    ~List() {
-        if (!isEmpty()) {
-            std::cout << "Destroying nodes...\n";
-
-            ListNode<NODETYPE>* tempPtr{nullptr};
-            ListNode<NODETYPE>* currentPtr{firstPtr};
-
-            while (currentPtr != nullptr) {
-                tempPtr = currentPtr;
-                currentPtr = currentPtr->nextPtr;
-                delete tempPtr;
-            }
-        }
-
-        std::cout << "All nodes destroyed\n\n";
-    };
-
-    void insertAtFront(const NODETYPE& value) {
-        ListNode<NODETYPE>* newPtr{getNewNode(value)};
-
-        if (isEmpty()) {
-            firstPtr = newPtr;
-            lastPtr = newPtr;
-        } else {
-            newPtr->nextPtr = firstPtr;
-            firstPtr = newPtr;
-        }
-    };
-
-    void insertAtBack(const NODETYPE& value) {
-        ListNode<NODETYPE>* newPtr{getNewNode(value)};
-
-        if (isEmpty()) {
-            firstPtr = newPtr;
-            lastPtr = newPtr;
-        } else {
-            lastPtr->nextPtr = newPtr;
-            lastPtr = newPtr;
-        }
-    };
-
-    bool removeFromFront(NODETYPE& value) {
-        if (isEmpty()) {
-            return false;
-        }
-
-        ListNode<NODETYPE>* tempPtr{firstPtr};
-
-        if (firstPtr == lastPtr) {
-            firstPtr = nullptr;
-            lastPtr = nullptr;
-        } else {
-            firstPtr = firstPtr->nextPtr;
-        }
-
-        value = tempPtr->getData();
-        delete tempPtr;
-        return true;
-    };
-
-    bool removeFromBack(NODETYPE& value) {
-        if (isEmpty()) {
-            return false;
-        }
-
-        ListNode<NODETYPE>* tempPtr{lastPtr};
-        ListNode<NODETYPE>* currentPtr{firstPtr};
-
-        if (firstPtr == lastPtr) {
-            firstPtr = nullptr;
-            lastPtr = nullptr;
-        } else {
-            while (currentPtr->nextPtr != lastPtr) {
-                currentPtr = currentPtr->nextPtr;
-            }
-
-            lastPtr = currentPtr;
-            currentPtr->nextPtr = nullptr;
-        }
-
-        value = tempPtr->getData();
-        delete tempPtr;
-        return true;
-    };
-
-    [[nodiscard]] bool isEmpty() const {
-        return firstPtr == nullptr;
-    };
-
-    void print() const {
-        if (isEmpty()) {
-            std::cout << "The list is empty.\n";
-        }
-
-        std::cout << "List:";
-
-        ListNode<NODETYPE>* currentPtr{firstPtr};
-
-        while (currentPtr != nullptr) {
-            std::cout << ' ' << currentPtr->getData();
-            currentPtr = currentPtr->nextPtr;
-        }
-
-        std::cout << "\n\n";
-    };
+    ~List();
+    void insertAtFront(const NODETYPE& value);
+    void insertAtBack(const NODETYPE& value);
+    bool removeFromFront(NODETYPE& value);
+    bool removeFromBack(NODETYPE& value);
+    [[nodiscard]] bool isEmpty() const;
+    void print() const;
 
     // extra function for exercise 19-07
-    void insertNode(ListNode<NODETYPE>* nodeBefore, const NODETYPE& value) {
-        ListNode<NODETYPE>* newPtr{getNewNode(value)};
-
-        if (isEmpty()) {
-            firstPtr = newPtr;
-            lastPtr = newPtr;
-        }
-
-        newPtr->nextPtr = nodeBefore->nextPtr;
-        nodeBefore->nextPtr = newPtr;
-    }
+    void insertNode(ListNode<NODETYPE>* nodeBefore, const NODETYPE& value);
 
     // extra function for exercise 19-08
-    void insertOrderedNode(const NODETYPE& value) {
-        if (isEmpty()) {
-            ListNode<NODETYPE>* newPtr{getNewNode(value)};
-            firstPtr = newPtr;
-            lastPtr = newPtr;
-            return;
-        }
-
-        if (firstPtr->getData() >= value) {
-            insertAtFront(value);
-            return;
-        }
-
-        if (lastPtr->getData() <= value) {
-            insertAtBack(value);
-            return;
-        }
-
-        ListNode<NODETYPE>* currentPtr{firstPtr};
-        while (true) {
-            if (currentPtr->nextPtr->getData() >= value) {
-                insertNode(currentPtr, value);
-                return;
-            }
-
-            currentPtr = currentPtr->nextPtr;
-        }
-    }
+    void insertOrderedNode(const NODETYPE& value);
 
 private:
     ListNode<NODETYPE>* firstPtr{nullptr};
@@ -175,5 +40,161 @@ private:
         return new ListNode<NODETYPE>{value}; // new syntax returns a pointer to the object
     }
 };
+
+template<typename NODETYPE>
+List<NODETYPE>::~List() {
+    if (!isEmpty()) {
+        std::cout << "Destroying nodes...\n";
+
+        ListNode<NODETYPE>* tempPtr{nullptr};
+        ListNode<NODETYPE>* currentPtr{firstPtr};
+
+        while (currentPtr != nullptr) {
+            tempPtr = currentPtr;
+            currentPtr = currentPtr->nextPtr;
+            delete tempPtr;
+        }
+    }
+
+    std::cout << "All nodes destroyed\n\n";
+};
+
+template<typename NODETYPE>
+void List<NODETYPE>::insertAtFront(const NODETYPE& value) {
+    ListNode<NODETYPE>* newPtr{getNewNode(value)};
+
+    if (isEmpty()) {
+        firstPtr = newPtr;
+        lastPtr = newPtr;
+    } else {
+        newPtr->nextPtr = firstPtr;
+        firstPtr = newPtr;
+    }
+};
+
+template<typename NODETYPE>
+void List<NODETYPE>::insertAtBack(const NODETYPE& value) {
+    ListNode<NODETYPE>* newPtr{getNewNode(value)};
+
+    if (isEmpty()) {
+        firstPtr = newPtr;
+        lastPtr = newPtr;
+    } else {
+        lastPtr->nextPtr = newPtr;
+        lastPtr = newPtr;
+    }
+};
+
+template<typename NODETYPE>
+bool List<NODETYPE>::removeFromFront(NODETYPE& value) {
+    if (isEmpty()) {
+        return false;
+    }
+
+    ListNode<NODETYPE>* tempPtr{firstPtr};
+
+    if (firstPtr == lastPtr) {
+        firstPtr = nullptr;
+        lastPtr = nullptr;
+    } else {
+        firstPtr = firstPtr->nextPtr;
+    }
+
+    value = tempPtr->getData();
+    delete tempPtr;
+    return true;
+};
+
+template<typename NODETYPE>
+bool List<NODETYPE>::removeFromBack(NODETYPE& value) {
+    if (isEmpty()) {
+        return false;
+    }
+
+    ListNode<NODETYPE>* tempPtr{lastPtr};
+    ListNode<NODETYPE>* currentPtr{firstPtr};
+
+    if (firstPtr == lastPtr) {
+        firstPtr = nullptr;
+        lastPtr = nullptr;
+    } else {
+        while (currentPtr->nextPtr != lastPtr) {
+            currentPtr = currentPtr->nextPtr;
+        }
+
+        lastPtr = currentPtr;
+        currentPtr->nextPtr = nullptr;
+    }
+
+    value = tempPtr->getData();
+    delete tempPtr;
+    return true;
+};
+
+template<typename NODETYPE>
+[[nodiscard]] bool List<NODETYPE>::isEmpty() const {
+    return firstPtr == nullptr;
+};
+
+template<typename NODETYPE>
+void List<NODETYPE>::print() const {
+    if (isEmpty()) {
+        std::cout << "The list is empty.\n";
+    }
+
+    std::cout << "List:";
+
+    ListNode<NODETYPE>* currentPtr{firstPtr};
+
+    while (currentPtr != nullptr) {
+        std::cout << ' ' << currentPtr->getData();
+        currentPtr = currentPtr->nextPtr;
+    }
+
+    std::cout << "\n\n";
+};
+
+template<typename NODETYPE>
+void List<NODETYPE>::insertNode(ListNode<NODETYPE>* nodeBefore, const NODETYPE& value) {
+    ListNode<NODETYPE>* newPtr{getNewNode(value)};
+
+    if (isEmpty()) {
+        firstPtr = newPtr;
+        lastPtr = newPtr;
+    }
+
+    newPtr->nextPtr = nodeBefore->nextPtr;
+    nodeBefore->nextPtr = newPtr;
+}
+
+template<typename NODETYPE>
+void List<NODETYPE>::insertOrderedNode(const NODETYPE& value) {
+    if (isEmpty()) {
+        ListNode<NODETYPE>* newPtr{getNewNode(value)};
+        firstPtr = newPtr;
+        lastPtr = newPtr;
+        return;
+    }
+
+    if (firstPtr->getData() >= value) {
+        insertAtFront(value);
+        return;
+    }
+
+    if (lastPtr->getData() <= value) {
+        insertAtBack(value);
+        return;
+    }
+
+    ListNode<NODETYPE>* currentPtr{firstPtr};
+    while (true) {
+        if (currentPtr->nextPtr->getData() >= value) {
+            insertNode(currentPtr, value);
+            return;
+        }
+
+        currentPtr = currentPtr->nextPtr;
+    }
+}
 
 #endif // LIST_H
