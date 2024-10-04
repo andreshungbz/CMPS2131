63aad2edc289a664d660bad0523b32fb4f4d52dd
Andres Hung <andres.hung@outlook.com>
2024-10-04 15:09:00 -0600

    Fix typo

--- practice/queue/food-order-management-system/main.cpp
+++ practice/queue/food-order-management-system/main.cpp
@@ -116,7 +116,7 @@ public:
     // constructor
     explicit Order(std::string& name) : customerName(name) {}
 
-    // displays order and itesm in formatted fashion
+    // displays order and items in formatted fashion
     void display(int number) const {
         std::cout << (express ? "Express " : "") << "Order " << number << ") " << customerName << '\n';
         itemsList.displayItems(number);
