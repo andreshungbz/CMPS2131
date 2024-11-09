844b3bda29a92b274e3ac6bcda1494c9d68511a6
Andres Hung <andres.hung@outlook.com>
2024-11-09 17:03:03 -0600

    Do simple read of file

--- final-project/02-huffman-encoding/main.cpp
+++ final-project/02-huffman-encoding/main.cpp
@@ -1,6 +1,20 @@
+#include <fstream>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // open test file in binary mode to read file exactly as is stored
+    std::ifstream input{"../test/input.txt", std::ios::in | std::ios::binary};
+    // handle file open error
+    if (!input.is_open()) {
+        std::cout << "File Open Error\n";
+        return 1;
+    }
+
+    // read every character and print it
+    char character;
+    while (input.get(character)) { // .get() evaluates false after reading invalid character
+        std::cout << character;
+    }
+
     return 0;
 }
