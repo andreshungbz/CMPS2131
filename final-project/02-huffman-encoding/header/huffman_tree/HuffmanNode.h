670f8466e138da517d61f03b0e94d312f3d4a28d
Andres Hung <andres.hung@outlook.com>
2024-11-10 11:30:55 -0600

    Add explanation comments

--- final-project/02-huffman-encoding/header/huffman_tree/HuffmanNode.h
+++ final-project/02-huffman-encoding/header/huffman_tree/HuffmanNode.h
@@ -1,5 +1,12 @@
 // Huffman Node Header and Implementation
 
+// The HuffmanNode object is used to store the keys and their respective weights. It is used for both building
+// the priority queue and the Huffman Tree. To account for non-leaf nodes in the Huffman Tree, which do not
+// hold valid key values, std::optional<char> type is used as it offers a convenient way of explicitly
+// determining invalid values over '\0' or nullptr. A key with value std::nullopt indicates a non-leaf node.
+
+// https://en.cppreference.com/w/cpp/utility/optional
+
 #ifndef HUFFMAN_NODE_H
 #define HUFFMAN_NODE_H
 
@@ -13,8 +20,9 @@ public:
     int weight{};
     HuffmanNode* left{nullptr};
     HuffmanNode* right{nullptr};
-    HuffmanNode(char keyValue, int weightValue) : key(keyValue), weight(weightValue) {}
-    explicit HuffmanNode(int weightValue) : weight(weightValue) {}
+    // constructors
+    HuffmanNode(char keyValue, int weightValue) : key(keyValue), weight(weightValue) {} // for priority queue
+    explicit HuffmanNode(int weightValue) : weight(weightValue) {} // for building Huffman Tree
 };
 
 
