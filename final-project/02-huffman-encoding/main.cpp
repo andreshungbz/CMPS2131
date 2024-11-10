c75f2e82f2ccbe3e8dbfddf13b5c730d2a88185c
Andres Hung <andres.hung@outlook.com>
2024-11-10 12:26:40 -0600

    Test priority queue

--- final-project/02-huffman-encoding/main.cpp
+++ final-project/02-huffman-encoding/main.cpp
@@ -1,7 +1,26 @@
 #include <fstream>
 #include <iostream>
 
-#include "./hash_map/FrequencyHashMap.h"
+#include "hash_map/FrequencyHashMap.h"
+#include "priority_queue/PriorityQueue.h"
+
+void traverseBST(PriorityQueue& queue, const FrequencyHashNode* root) {
+    // base case
+    if (root == nullptr) {
+        return;
+    }
+
+    // traverse inorder, going through every node and populating the queue
+    queue.enqueue(root->key, root->frequency);
+    traverseBST(queue, root->left);
+    traverseBST(queue, root->right);
+}
+
+void populateQueue(PriorityQueue& queue, const FrequencyHashMap& hashMap) {
+    for (FrequencyHashNode* tree : hashMap.buckets) {
+        traverseBST(queue, tree);
+    }
+}
 
 int main() {
     // open test file in binary mode to read file exactly as is stored
@@ -20,6 +39,10 @@ int main() {
     while (input.get(character)) { // .get() evaluates false after reading invalid character
         hashMap.insertHashNode(character);
     }
+
+    PriorityQueue queue{};
+
+    populateQueue(queue, hashMap);
 
     return 0;
 }
