52d2f8a8ddeb2ef62087888eaf043e51ce1b1608
Andres Hung <andres.hung@outlook.com>
2024-11-11 14:29:28 -0600

    Create file path

--- final-project/02-huffman-encoding/main.cpp
+++ final-project/02-huffman-encoding/main.cpp
@@ -1,10 +1,24 @@
 // https://web.stanford.edu/class/archive/cs/cs106b/cs106b.1234/resources/huffman.html
 
+#include <string>
+
 #include "huffman_tree/HuffmanTree.h"
 
 int main() {
+    std::string path{".."};
+
+    // create relative path for Windows and Unix/Linux
+    #if defined(_WIN32)
+        path += "\\test\\";
+    #else
+        path +="/test/";
+    #endif
+
+    // append word list file
+    path += "input.txt";
+
     // create Huffman Tree
-    HuffmanTree huffmanTree{"../test/input.txt"};
+    HuffmanTree huffmanTree{path};
 
     return 0;
 }
