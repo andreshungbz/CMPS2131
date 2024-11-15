ef0c0861eb3479cea7b108d4b00a5b15d3b3878b
Andres Hung <andres.hung@outlook.com>
2024-11-15 15:15:31 -0600

    Add comments and explanations. Reorder functions

--- final-project/02-huffman-encoding/header/FileUtils.h
+++ final-project/02-huffman-encoding/header/FileUtils.h
@@ -1,13 +1,24 @@
+// File Utilities Header
+
+// This module defines a number of functions for getting information about a file on the system. Two implementations
+// are accounted for depending on whether the system from which this project is compiled on supports the <filesystem>
+// library in the C++ 17 standard. The MinGW compiler on the school computers support the C++ 17 standard, but not
+// <filesystem>, so POSIX functions were found as a fallback alternative. Platform-specific code is used: on Windows
+// systems _fullpath() and on Linux/macOS realpath().
+
+// Preprocessor directives are used to manage this code, with use of __has_defined to check for <filesystem> and
+// _WIN32 to check for compilation on Windows systems.
+
 #ifndef FILE_UTILS_H
 #define FILE_UTILS_H
 
 
 #include <string>
 
-std::size_t getFileSize(const std::string& path);
-std::string getDirectory(const std::string& path);
 std::string getFileName(const std::string& path);
 std::string getFileExtension(const std::string& path);
+std::size_t getFileSize(const std::string& path);
+std::string getDirectory(const std::string& path);
 
 
-#endif //FILE_UTILS_H
+#endif // FILE_UTILS_H
--- final-project/02-huffman-encoding/src/FileUtils.cpp
+++ final-project/02-huffman-encoding/src/FileUtils.cpp
@@ -1,5 +1,8 @@
+// File Utilities Implementation
+
 #include "FileUtils.h"
 
+// determine if the system has the <filesystem> library
 #if __has_include(<filesystem>)
     #include <filesystem>
     #define USE_FILESYSTEM 1
@@ -8,15 +11,6 @@
 #endif
 
 #if USE_FILESYSTEM // functions using <filesystem>
-
-    std::size_t getFileSize(const std::string& path) {
-        return std::filesystem::file_size(path);
-    }
-
-    std::string getDirectory(const std::string& path) {
-        std::filesystem::path filePath{path};
-        return std::filesystem::canonical(filePath.parent_path()).string();
-    }
 
     std::string getFileName(const std::string& path) {
         std::filesystem::path filePath{path};
@@ -28,6 +22,15 @@
         return filePath.extension().string();
     }
 
+    std::size_t getFileSize(const std::string& path) {
+        return std::filesystem::file_size(path);
+    }
+
+    std::string getDirectory(const std::string& path) {
+        std::filesystem::path filePath{path};
+        return std::filesystem::canonical(filePath.parent_path()).string();
+    }
+
 #else // functions using POSIX
 
 #include <sys/stat.h>
@@ -36,6 +39,17 @@
     #include <windows.h>
     #include <stdlib.h>
 #endif
+
+std::string getFileName(const std::string& path) {
+        size_t start = path.find_last_of("/\\") + 1;
+        size_t end = path.find_last_of('.');
+        return path.substr(start, end - start);
+    }
+
+std::string getFileExtension(const std::string& path) {
+        size_t pos = path.find_last_of('.');
+        return (std::string::npos == pos) ? "" : path.substr(pos);
+    }
 
 std::size_t getFileSize(const std::string& path) {
     struct stat statBuf{};
@@ -61,17 +75,6 @@ std::string getDirectory(const std::string& path) {
     std::string fullPath(absPath);
     size_t pos = fullPath.find_last_of("/\\");
     return (std::string::npos == pos) ? "" : fullPath.substr(0, pos);
-}
-
-std::string getFileName(const std::string& path) {
-    size_t start = path.find_last_of("/\\") + 1;
-    size_t end = path.find_last_of('.');
-    return path.substr(start, end - start);
-}
-
-std::string getFileExtension(const std::string& path) {
-    size_t pos = path.find_last_of('.');
-    return (std::string::npos == pos) ? "" : path.substr(pos);
 }
 
 #endif
